5ca7e8f3de277c595551fef0a8b9d74236ccecab
Kyler Robison <63178384+krobison10@users.noreply.github.com>
2022-03-10 10:10:30 -0800

    Initial commit

---
+++ src/GUICalculator/Main.java
@@ -0,0 +1,16 @@
+package GUICalculator;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @version 0.1
+ * @author Kyler Robison
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
