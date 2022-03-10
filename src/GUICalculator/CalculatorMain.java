89ce81f64fa5b37a46ddc9b7d6d966f0a0f12639
Kyler Robison <63178384+krobison10@users.noreply.github.com>
2022-03-10 15:37:48 -0800

    Functioning GUI

--- src/GUICalculator/CalculatorMain.java
+++ src/GUICalculator/CalculatorMain.java
@@ -1,61 +1,83 @@
 package GUICalculator;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
- * @version 0.1
+ * @version 1.0 - does nothing but looks pretty
  * @author Kyler Robison
  */
 
 public class CalculatorMain
 {
-
+    //declare main window JFrame
     private static final JFrame calcWindow = new JFrame();
+    private static final int FONT_SIZE = 20;
 
     public static void main(String[] args)
     {
         EventQueue.invokeLater(CalculatorMain::initializeWindow);
-
-
-
     }
     public static void buildGUI()
     {
+        //Initialize the main calculator panel
         JPanel calcPanel = new JPanel();
         calcPanel.setLayout(new GridBagLayout());
+        calcPanel.setBorder(new EmptyBorder(6, 6, 6, 6));
+        calcPanel.setBackground(Color.DARK_GRAY);
 
-        //Button set
+        //Define set containing the buttons
         Map<String, JButton> buttons = initializeButtons("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-        "C", "DEL", "\u00F7", "-", "X", "+", "=", ".", "\u221A", ("X"+"\u00B2"), "1/x");
+        "C", "DEL", "\u00F7", "-", "X", "+", "=", ".", "\u221A", ("x"+"\u00B2"), "1/x");
 
+        //Set general grid cell attributes
         GridBagConstraints c = new GridBagConstraints();
-
-        //Number Buttons
         c.fill = GridBagConstraints.BOTH;
         c.weightx = 1;
         c.weighty = 1;
-        c.fill = GridBagConstraints.BOTH;
+        c.insets = new Insets(2, 2, 2, 2);
 
 
         //Row 1
         c.gridx = 0;
         c.gridy = 0;
         c.gridwidth = 4;
-        var calcLabel = new JTextField( 20);
-        calcLabel.setEnabled(false);
 
+        //Set up label
+        var calcLabel = new JTextField("Hello World!");
+        calcLabel.setEditable(false);
+        calcLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        calcLabel.setFont(new Font("Arial", Font.BOLD, 30));
+        calcLabel.setMargin(new Insets(-8, 0, -8, 0));
+
+        //add to grid
         calcPanel.add(calcLabel, c);
         c.gridwidth = 1;
 
         //Row 2
         c.gridy = 1;
         c.gridx = 0;
-        //radio buttons
+
+        //Initialize mini grid of radio buttons then add to cell
+        JPanel radioButtons = new JPanel(new GridLayout(2,1));
+        radioButtons.setBackground(Color.DARK_GRAY);
+
+        JRadioButton rd1 = new JRadioButton("on");
+        rd1.setFont(new Font("Arial", Font.BOLD, 15));
+        rd1.setForeground(Color.white);
+        rd1.setSelected(true);
+        radioButtons.add(rd1);
+
+        JRadioButton rd2 = new JRadioButton("off");
+        rd2.setFont(new Font("Arial", Font.BOLD, 15));
+        rd2.setForeground(Color.white);
+        radioButtons.add(rd2);
+
+        calcPanel.add(radioButtons, c);
+
+        //Proceed with row
         c.gridx = 1;
         calcPanel.add(buttons.get("C"), c);
         c.gridx = 2;
@@ -68,7 +90,7 @@ public class CalculatorMain
         c.gridx = 0;
         calcPanel.add(buttons.get("\u221A"), c);
         c.gridx = 1;
-        calcPanel.add(buttons.get("X"+"\u00B2"), c);
+        calcPanel.add(buttons.get("x"+"\u00B2"), c);
         c.gridx = 2;
         calcPanel.add(buttons.get("1/x"), c);
         c.gridx = 3;
@@ -98,6 +120,7 @@ public class CalculatorMain
 
         //Row 6
         c.gridy = 5;
+
         c.gridx = 0;
         calcPanel.add(buttons.get("1"), c);
         c.gridx = 1;
@@ -111,6 +134,7 @@ public class CalculatorMain
 
         //Row 7
         c.gridy = 6;
+
         c.gridx = 0;
         c.gridwidth = 2;
         calcPanel.add(buttons.get("0"), c);
@@ -118,25 +142,30 @@ public class CalculatorMain
         c.gridx = 2;
         calcPanel.add(buttons.get("."), c);
 
-
-
-        //Add parent panel to the JFrame window
+        //Add panel to the JFrame window
         calcWindow.add(calcPanel);
     }
     private static Map<String, JButton> initializeButtons(String... strings)
     {
+        //Loop through strings and create a hashmap of buttons
         Map<String, JButton> output = new HashMap<>();
         for (String value : strings)
-            output.put(value, new JButton(value));
+        {
+            JButton btn = new JButton(value);
+            btn.setForeground(Color.DARK_GRAY);
+            btn.setFont(new Font("Arial", Font.BOLD, FONT_SIZE));
+
+            output.put(value, btn);
+        }
         return output;
     }
     public static void initializeWindow()
     {
+        //Set attributes for the window, then call the GUI builder
         calcWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         calcWindow.setSize(300,500);
         calcWindow.setResizable(false);
         calcWindow.setTitle("Calculator");
-
         buildGUI();
         calcWindow.setVisible(true);
     }
